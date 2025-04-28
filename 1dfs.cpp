28cf67a080f88179acdf069b692cb1da3e384c98
hosteldon <m64395135@gmail.com>
2025-04-28 23:31:33 +0530

    Update 1dfs.cpp

--- 1dfs.cpp
+++ 1dfs.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <stack>
+#include <omp.h> // Include OpenMP
 using namespace std;
+
 const int MAX = 100;
 vector<int> graph[MAX];
 bool visited[MAX];
@@ -9,15 +11,34 @@ bool visited[MAX];
 void dfs(int node) {
     stack<int> s;
     s.push(node);
+
     while (!s.empty()) {
-        int curr_node = s.top();
-        s.pop();
+        int curr_node;
+
+        #pragma omp critical
+        {
+            curr_node = s.top();
+            s.pop();
+        }
+
         if (!visited[curr_node]) {
-            visited[curr_node] = true;
-            for (int i = 0; i < graph[curr_node].size(); i++) {
+            #pragma omp critical
+            {
+                visited[curr_node] = true;
+                cout << curr_node << " ";
+            }
+
+            int neighbors = graph[curr_node].size();
+
+            // Parallelize pushing neighbors
+            #pragma omp parallel for
+            for (int i = 0; i < neighbors; i++) {
                 int adj_node = graph[curr_node][i];
                 if (!visited[adj_node]) {
-                    s.push(adj_node);
+                    #pragma omp critical
+                    {
+                        s.push(adj_node);
+                    }
                 }
             }
         }
@@ -26,25 +47,29 @@ void dfs(int node) {
 
 int main() {
     int n, m, start_node;
-    cout << "Enter number of nodes, edges and starting node: " << flush;
+    cout << "Enter number of nodes, edges and starting node: ";
     cin >> n >> m >> start_node;
+
     cout << "Enter " << m << " edges (u v):" << endl;
     for (int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
         graph[u].push_back(v);
-        graph[v].push_back(u);
+        graph[v].push_back(u); // Undirected graph
     }
+
     for (int i = 0; i < n; i++) {
         visited[i] = false;
     }
-    dfs(start_node);
+
     cout << "\nNodes visited in DFS order starting from " << start_node << ":" << endl;
-    for (int i = 0; i < n; i++) {
-        if (visited[i]) {
-            cout << i << " ";
-        }
+    
+    #pragma omp parallel
+    {
+        #pragma omp single
+        dfs(start_node);
     }
+
     cout << endl;
     return 0;
 }
